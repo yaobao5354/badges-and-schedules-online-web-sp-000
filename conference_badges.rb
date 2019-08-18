7e659aa9242f2cc11a25f152800fe879e686c8ec
yaoyu bao <yaobao5354@gmail.com>
2019-08-18 21:24:23 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,9 @@
 # Write your code here.
+require 'pry'
+def badge_maker(name)
+    "Hello, my name is #{name}."
+end 
+
+def batch_badge_creator(names)
+    names.collect{|name| "Hello, my name is #{name}."}
+end
