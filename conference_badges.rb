d538ce7ea683996f37dca8f41e29541a93b36858
yaoyu bao <yaobao5354@gmail.com>
2019-08-18 21:46:57 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -11,3 +11,7 @@ end
 def assign_rooms(attendees)
    attendees.collect.with_index{|attendee, index| "Hello, #{attendee}! You'll be assigned to room #{index+1}!"}
 end
+
+def printer(attendees)
+   batch_badge_creator(attendees).each{|badge| puts "#{badge}"}
+end 
