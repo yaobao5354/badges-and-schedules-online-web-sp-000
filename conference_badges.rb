e384b806750edf113fb53e2cf22f781577cb0872
yaoyu bao <yaobao5354@gmail.com>
2019-08-18 21:54:36 +0000

    Done.

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,18 @@
 # Write your code here.
+require 'pry'
+def badge_maker(name)
+    "Hello, my name is #{name}."
+end 
+
+def batch_badge_creator(names)
+    names.collect{|name| "Hello, my name is #{name}."}
+end
+
+def assign_rooms(attendees)
+   attendees.collect.with_index{|attendee, index| "Hello, #{attendee}! You'll be assigned to room #{index+1}!"}
+end
+
+def printer(attendees)
+  batch_badge_creator(attendees).each{|attendee| puts "#{attendee}"}
+  assign_rooms(attendees).each{|attendee| puts "#{attendee}"}
+end 
